23a43b2af248cac36bec43fc43dd67f3cf5c09ac
Niki Stefanopoulou <142924094+NikStef18@users.noreply.github.com>
2023-09-26 21:10:18 +0100

    Update test_integration.py

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -28,4 +28,30 @@ def test_mark_complete_task1_and_return_a_list_of_task2():
     task_1.mark_complete()
     assert todo_list.incomplete() == [task_2]
 
+"""
+Given we have two tasks in todo list
+And we mark complete task_1
+when we call #complete it returns a list with task_1 in it
+"""
+def test_mark_complete_task1_and_return_a_list_of_task1():
+    todo_list = TodoList()
+    task_1 = Todo("My task 1")
+    task_2 = Todo("My task 2")
+    todo_list.add(task_1)
+    todo_list.add(task_2)
+    task_1.mark_complete()
+    assert todo_list.complete() == [task_1]
 
+"""
+Given we have two tasks in todo list
+And we mark complete all tasks
+when we call #give_up
+"""
+def test_all_tasks_complete_when_give_up():
+    todo_list = TodoList()
+    task_1 = Todo("My task 1")
+    task_2 = Todo("My task 2")
+    todo_list.add(task_1)
+    todo_list.add(task_2)
+    todo_list.give_up() 
+    assert all(task.complete for task in todo_list.todos)
